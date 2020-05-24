9c6f32a879722c46e64287d6642623181720ea84
Zeraati <zeraatimail@gmail.com>
2020-05-24 04:33:13 +0430

    ***

---
+++ HPlusSportAPI/Models/Ordered.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HPlusSportAPI.Models
+{
+    public class Ordered
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public int UserId { get; set; }
+        [JsonIgnore]
+        public virtual User User { get; set; }
+        
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ HPlusSportAPI/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HPlusSportAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public bool CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category Category { get; set; }
+    }
+}
